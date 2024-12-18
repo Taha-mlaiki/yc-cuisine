2cf2ccdeaf3a76f5947a1bebc7b4426082249ef5
taha <mlaikitaha29@gmail.com>
2024-12-18 21:51:37 +0100

    seperate the navbar and footer into reusable components

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-xWtA/MSrKnMtS/ENhFqDx65VRbTE4U3KyiiIFeNrL4a6PyVrfEam+VGQw6LH0znQPowVUS2VhtGK957ZWNMI/" crossorigin="anonymous">
+    <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#f57c3b',
+          }
+        }
+      }
+    }
+  </script>
+</head>
+<body>
